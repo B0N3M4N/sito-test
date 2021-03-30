24d29355f8bf1f6ec85fb819f466a9d6e547f337
giorg <giorg@Lupo>
2021-03-30 12:04:50 +0200

    Prova 57!

--- TestGhub/src/NoNecessarioMaObbligatorioHelloWorld.java
+++ TestGhub/src/NoNecessarioMaObbligatorioHelloWorld.java
@@ -9,24 +9,19 @@ public class NoNecessarioMaObbligatorioHelloWorld {
 
 		System.out.println("Sono  alberto!");
 
-<<<<<<< HEAD
+
 		System.out.println("sono fou e questo commento probabilmente non vedrà luce");
-=======
+
 
 		System.err.println("sono fou e questo commento probabilmente non vedrà luce");
->>>>>>> branch 'main' of https://github.com/B0N3M4N/sito-test.git
+
 		System.out.println("Sono Francesco!");
-<<<<<<< HEAD
-=======
+
 
 		System.err.println("sono fou e questo commento ha visto la luce");
->>>>>>> branch 'main' of https://github.com/B0N3M4N/sito-test.git
 
-<<<<<<< HEAD
 		System.out.println("sono fou e questo commento ha visto la luce");
 
-=======
 		System.out.println("Para bailar la bamba");
->>>>>>> branch 'main' of https://github.com/B0N3M4N/sito-test.git
 	}
 }
