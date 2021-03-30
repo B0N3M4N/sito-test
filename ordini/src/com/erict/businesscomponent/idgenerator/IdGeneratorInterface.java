538b92370f09f5d2321683a05174b85923a8880b
Eric <eric.trapani@live.it>
2021-03-30 10:56:35 +0200

    Delete ordini directory

--- ordini/src/com/erict/businesscomponent/idgenerator/IdGeneratorInterface.java
+++
@@ -1,9 +0,0 @@
-package com.erict.businesscomponent.idgenerator;
-
-import java.io.IOException;
-
-import com.erict.architecture.dao.DAOException;
-
-public interface IdGeneratorInterface {
-	long getNextId() throws ClassNotFoundException, DAOException, IOException;
-}
