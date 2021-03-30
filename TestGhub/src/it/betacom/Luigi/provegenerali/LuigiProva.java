cd4823047a03416d47ec3dc2ce5705ee11e48c40
LuigiDSantos <LuigiDSantos@users.noreply.github.com>
2021-03-30 12:45:48 +0200

    re-commit classe inutile

---
+++ TestGhub/src/it/betacom/Luigi/provegenerali/LuigiProva.java
@@ -0,0 +1,13 @@
+package it.betacom.Luigi.provegenerali;
+
+public class LuigiProva {
+	public static void main(String[] args) {
+		System.out.println("Questa classe non ha senso");
+		
+		for(int i = 0; i < 6; i++) {
+			System.out.println(i + ", Scelgo te!!");
+		}
+		
+		System.out.println("Non ha senso guardare questa classe");
+	}
+}
